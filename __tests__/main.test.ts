67ac437b3423410722cb4b4479e3dab5d0dc385a
Alexander Daza <dev.alexander.daza@gmail.com>
2023-11-12 17:05:58 -0500

    chore: base unit test with jest implemented

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,42 +1,54 @@
-import { Delays, greeter } from '../src/main.js';
+import { MultiCaptchaSolver } from "../src/main.js";
+import { ECaptchaSolverService } from "../src/mcs.enum.js";
+import { IMultiCaptchaSolverOptions } from "../src/mcs.interface.js";
 
-describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
+// Mock para AntiCaptchaService
+jest.mock('./services/anticaptcha.service', () => {
+  return {
+    AntiCaptchaService: jest.fn().mockImplementation(() => ({
+      getBalance: jest.fn().mockResolvedValue(100),  // Puedes cambiar este valor según tus necesidades
+      solveImageCaptcha: jest.fn().mockResolvedValue('52848783'),
+    })),
+  };
+});
 
-  let timeoutSpy: jest.SpyInstance;
+describe('MultiCaptchaSolver', () => {
+  const validOptions: IMultiCaptchaSolverOptions = {
+    apiKey: 'your-api-key',
+    captchaService: ECaptchaSolverService.AntiCaptcha,
+  };
 
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
-
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
+  // Prueba constructor con opciones válidas
+  it('should create an instance with valid options', () => {
+    const multiCaptchaSolver = new MultiCaptchaSolver(validOptions);
+    expect(multiCaptchaSolver).toBeInstanceOf(MultiCaptchaSolver);
   });
 
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
+  // Prueba constructor sin opciones
+  it('should throw an error if no options provided', () => {
+    expect(() => new MultiCaptchaSolver(null)).toThrowError('No valid options provided.');
   });
 
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
+  // Prueba getBalance
+  it('should get the balance of the captcha solver account', async () => {
+    const multiCaptchaSolver = new MultiCaptchaSolver(validOptions);
+    const balance = await multiCaptchaSolver.getBalance();
+    expect(balance).toEqual(expect.any(Number));
   });
 
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
+  // Prueba solveImageCaptcha
+  it('should solve an image captcha', async () => {
+    const multiCaptchaSolver = new MultiCaptchaSolver(validOptions);
+    const captchaSolution = await multiCaptchaSolver.solveImageCaptcha('base64string');
+    expect(captchaSolution).toEqual('52848783');
+  });
+
+  // Prueba constructor con servicio de captcha no válido
+  it('should throw an error if an invalid captcha service is provided', () => {
+    const invalidOptions: IMultiCaptchaSolverOptions = {
+      apiKey: 'your-api-key',
+      captchaService: 'InvalidService' as ECaptchaSolverService,
+    };
+    expect(() => new MultiCaptchaSolver(invalidOptions)).toThrowError('Invalid captcha service.');
   });
 });
